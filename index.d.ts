67ddef5ab1117ae6aeccc302f35025d903363d0d
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2017-11-23 22:37:52 +0000

    Task: Update typings (#3)

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,20 @@
 
+type ProjectModuleKey = 'literals' | 'regex';
+interface ProjectModule {
+  literals: string[];
+  regex: string[];
+}
+
+type ProjectModulesKey = 'PROJECT' | 'WEBPACK' | 'BABEL' | 'TYPESCRIPT' | 'STYLE' | 'TESTS';
+interface ProjectModules {
+  PROJECT: ProjectModule;
+  WEBPACK: ProjectModule;
+  BABEL: ProjectModule;
+  TYPESCRIPT: ProjectModule;
+  STYLE: ProjectModule;
+  TESTS: ProjectModule;
+}
+
 declare module 'project-packages' {
   type ProjectModuleKey = 'literals' | 'regex';
   interface ProjectModule {
